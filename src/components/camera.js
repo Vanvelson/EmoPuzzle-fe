9cd935432e0586ef8603e800bd01440661c3b1ff
Лобур Іван Володимирович <ivanlobur5@gmail.com>
2025-05-23 18:49:39 +0300

    main_changes_4.0

--- src/components/camera.js
+++ src/components/camera.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../App.css";
-
+ 
 const tasks = {
   1: {
     title: "A Sweet Surprise",
