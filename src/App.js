d757f92ee1d058621ceb79485665a8de7d4b9f6e
Лобур Іван Володимирович <ivanlobur5@gmail.com>
2025-05-23 18:39:40 +0300

    main_changes_3.0

--- src/App.js
+++ src/App.js
@@ -5,12 +5,6 @@ import Login from "./components/Login";
 import FirstLevel from "./components/FirstLevel";
 import ThirdLevel from "./components/ThirdLevel";
 import Module_1_first from './components/module_1_first/module_1_first';
-import Module_1_second from "./components/module_1_second/module_1_second";
-import Module_1_third from "./components/module_1_third/module_1_third";
-import Module_1_fourth from "./components/module_1_fourth/module_1_fourth";
-import Scene from "./components/scene";
-import Scene_1 from "./components/scene_1";
-import Scene_2 from "./components/scene_2";
 import Camera from "./components/camera";
 import "./App.css"; 
 
@@ -23,12 +17,6 @@ const App = () => {
         <Route path="/firstlevel" element={<FirstLevel />} />
         <Route path="/Thirdlevel" element={<ThirdLevel />} />
         <Route path="/Module_1_first" element={<Module_1_first />} />
-        <Route path="/Module_1_second" element={<Module_1_second />} />
-        <Route path="/Module_1_third" element={<Module_1_third />} />
-        <Route path="/Module_1_fourth" element={<Module_1_fourth />} />
-        <Route path="/scene" element={<Scene />} />
-        <Route path="/scene_1" element={<Scene_1 />} />
-        <Route path="/scene_2" element={<Scene_2 />} />
         <Route path="/camera" element={<Camera />} />
       </Routes>
     </Router>
